bb84d63bafe84a7874b7548f32d7377550f5c659
GeekNoob04 <harshitbudhraja0@gmail.com>
2025-09-25 20:42:10 +0530

    graphql helper function with axios post call

--- lib/leetcode.ts
+++ lib/leetcode.ts
@@ -1,2 +1,41 @@
 import axios from "axios";
 
+export async function fetchLeetCodeStats(username: string) {
+    const query = `
+    query getUserProfile($username: String!) {
+        matchedUser(username: $username) {
+        username
+            profile {
+                ranking
+                reputation
+            }
+            submitStats: submitStatsGlobal {
+            acSubmissionNum {
+                difficulty
+                count
+            }
+            totalSubmissionNum {
+                difficulty
+                count
+            }   
+        }
+    }
+}`;
+    const variables = { username };
+    const res = await axios.post(
+        "https://leetcode.com/graphql",
+        {
+            query,
+            variables,
+        },
+        {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }
+    );
+    if (res.data.errors) {
+        throw new Error(res.data.errors[0].message);
+    }
+    return res.data.data;
+}
