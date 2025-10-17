abd0be5fc0f246f5e05017ac1658358d9fc95543
GeekNoob04 <harshitbudhraja0@gmail.com>
2025-10-18 02:27:25 +0530

    api function completed

--- lib/leetcode.ts
+++ lib/leetcode.ts
@@ -113,4 +113,38 @@ export async function fetchLeetCodeTopicStats(username: string) {
             }
         }
     }`;
+    const variables = { username };
+    const res = await axios.post(
+        "https://leetcode.com/graphql",
+        {
+            query,
+            variables,
+        },
+        { headers: { "Content-Type": "application/json" } }
+    );
+    if (res.data.errors) {
+        throw new Error(res.data.errors[0].message);
+    }
+    const tagData = res.data.data?.matchedUser?.tagProblemCounts;
+    if (!tagData) {
+        throw new Error("No topic data found");
+    }
+    const allTopics = [
+        ...tagData.fundamental,
+        ...tagData.intermediate,
+        ...tagData.advanced,
+    ];
+    return allTopics
+        .filter((t: { problemsSolved: number }) => t.problemsSolved > 0)
+        .map(
+            (t: {
+                tagName: string;
+                tagSlug: string;
+                problemsSolved: number;
+            }) => ({
+                name: t.tagName,
+                slug: t.tagSlug,
+                solved: t.problemsSolved,
+            })
+        );
 }
